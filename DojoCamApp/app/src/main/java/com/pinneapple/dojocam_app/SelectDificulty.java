d4c22eae75cfd3efb6efc546fd247c2a613f5863
bsolar.v <bastian.solar.v@gmail.com>
2021-07-19 15:48:34 -0400

    Bug Fixing

--- DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/SelectDificulty.java
+++ DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/SelectDificulty.java
@@ -1,10 +1,11 @@
-package com.pinneapple.dojocam;
+package com.pinneapple.dojocam_app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
