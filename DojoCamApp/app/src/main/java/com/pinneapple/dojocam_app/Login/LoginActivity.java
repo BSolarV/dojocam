cbb73cc22d0667da542db661cac232b03ce19c8f
bsolar.v <bastian.solar.v@gmail.com>
2021-09-15 01:31:27 -0300

    Login y Register Views (Not Working)

---
+++ DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/Login/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.pinneapple.dojocam_app.Login;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.pinneapple.dojocam_app.R;
+import com.pinneapple.dojocam_app.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
