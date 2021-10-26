c32e770d93924e1365641f3f62a7ecc0d7bac47b
bsolar.v <bastian.solar.v@gmail.com>
2021-10-26 15:28:49 -0300

    Data Jsons Promediated

--- ExtractingData/.transform.sh
+++ ExtractingData/.transform.sh
@@ -1,6 +1,6 @@
 
 tensorflowjs_converter --input_format=tfjs_layers_model --output_format=keras_saved_model  ./$1/model.json ./$1-keras
 
-python3 transformer.py $1
+python3 .transformer.py $1
 
 echo done
