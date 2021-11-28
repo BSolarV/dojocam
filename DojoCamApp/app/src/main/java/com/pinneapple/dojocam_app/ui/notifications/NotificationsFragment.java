467956a23f0b07e98012493a81a15dc602845422
rhernan <rodrigo.hernandezgo@sansano.usm.cl>
2021-11-28 06:35:11 -0300

    Fixed label name

--- DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/ui/notifications/NotificationsFragment.java
+++ DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/ui/notifications/NotificationsFragment.java
@@ -16,9 +16,13 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.pinneapple.dojocam_app.R;
 import com.pinneapple.dojocam_app.databinding.FragmentNotificationsBinding;
@@ -96,6 +100,7 @@ public class NotificationsFragment extends Fragment {
                 semana.add(new BarEntry(6, 25));
                 semana.add(new BarEntry(7, 28));
 
+
                 BarDataSet barDataSet = new BarDataSet(semana, "Puntajes Semanales");
 
                 barDataSet.setColor(Color.CYAN);
@@ -105,7 +110,101 @@ public class NotificationsFragment extends Fragment {
                 BarData barDataweek = new BarData(barDataSet);
                 barChart.setFitBars(true);
                 barChart.setData(barDataweek);
-                barChart.getDescription().setText("Puntajes Semanales");
+                ArrayList<String> xLabel = new ArrayList<>();
+                xLabel.add("Lunes");
+                xLabel.add("Martes");
+                xLabel.add("Miercoles");
+                xLabel.add("Jueves");
+                xLabel.add("Viernes");
+                xLabel.add("Sabado");
+                xLabel.add("Domingo");
+
+                XAxis xAxis = barChart.getXAxis();
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawGridLines(false);
+                xAxis.setValueFormatter(new ValueFormatter() {
+                    @Override
+                    public String getAxisLabel(float value, AxisBase axis) {
+                        return xLabel.get((int)value-1);
+                    }
+                });
+
+                barChart.getDescription().setText("Puntaje Semanal");
+                barChart.animateY(2000);
+            }
+        });
+        radio_month.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<BarEntry> mes = new ArrayList<>();
+                ArrayList<String> xLabel = new ArrayList<>();
+                xLabel.add("Enero");
+                xLabel.add("Febrero");
+                xLabel.add("Marzo");
+                xLabel.add("Abril");
+                xLabel.add("Mayo");
+                xLabel.add("Junio");
+                xLabel.add("Julio");
+                xLabel.add("Agosto");
+                xLabel.add("Septiembre");
+                xLabel.add("Octubre");
+                xLabel.add("Noviembre");
+                xLabel.add("Diciembre");
+                XAxis xAxis = barChart.getXAxis();
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawGridLines(false);
+                xAxis.setValueFormatter(new ValueFormatter() {
+                    @Override
+                    public String getAxisLabel(float value, AxisBase axis) {
+                        return xLabel.get((int)value-1);
+                    }
+                });
+                mes.add(new BarEntry(1, 61));
+                mes.add(new BarEntry(2, 22));
+                mes.add(new BarEntry(3, 25));
+                mes.add(new BarEntry(4, 31));
+                mes.add(new BarEntry(5, 58));
+                mes.add(new BarEntry(6, 25));
+                mes.add(new BarEntry(7, 39));
+                mes.add(new BarEntry(8, 48));
+                mes.add(new BarEntry(9, 50));
+                mes.add(new BarEntry(10, 28));
+                mes.add(new BarEntry(11, 18));
+                mes.add(new BarEntry(12, 30));
+
+
+
+                BarDataSet barDataSet = new BarDataSet(mes, "Puntajes Mensuales");
+
+                barDataSet.setColor(Color.BLUE);
+                barDataSet.setValueTextColor(Color.BLACK);
+                barDataSet.setValueTextSize(16f);
+
+                BarData barDatamonth = new BarData(barDataSet);
+                barChart.setFitBars(true);
+                barChart.setData(barDatamonth);
+                barChart.getDescription().setText("Puntajes Mensuales");
+                barChart.animateY(2000);
+
+            }
+        });
+        radio_year.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<BarEntry> mes = new ArrayList<>();
+                mes.add(new BarEntry(2014, 61));
+                mes.add(new BarEntry(2015, 22));
+                mes.add(new BarEntry(3, 25));
+
+                BarDataSet barDataSet = new BarDataSet(mes, "Puntajes Mensuales");
+
+                barDataSet.setColor(Color.RED);
+                barDataSet.setValueTextColor(Color.BLACK);
+                barDataSet.setValueTextSize(16f);
+
+                BarData barDataweek = new BarData(barDataSet);
+                barChart.setFitBars(true);
+                barChart.setData(barDataweek);
                 barChart.animateY(2000);
             }
         });
