213725b79e5a8ecfb453b99d34a4cbb926112c8c
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2021-12-02 00:42:48 -0300

    ejercicio recomendado

--- DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/ui/dashboard/DashboardFragment.java
+++ DojoCamApp/app/src/main/java/com/pinneapple/dojocam_app/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,13 @@
 package com.pinneapple.dojocam_app.ui.dashboard;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +17,41 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.pinneapple.dojocam_app.R;
 import com.pinneapple.dojocam_app.databinding.FragmentDashboardBinding;
+import com.pinneapple.dojocam_app.objects.VideoInfo;
+import com.pinneapple.dojocam_app.objets.UserData;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
 public class DashboardFragment extends Fragment implements View.OnClickListener {
 
     private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private TextView name, dificultad;
+    private ImageView img;
+    String whereQ = "basico";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +60,51 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        DocumentReference userReference = db.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        userReference.get().addOnSuccessListener(command -> {
+                    UserData user = command.toObject(UserData.class);
+                    assert user != null;
+
+                    if( user.getLastExercise() == null ) {
+                        whereQ = "basico";
+                    } else {
+                        whereQ = user.getLastExercise();
+                    }
+
+                });
+
+
+        Task<QuerySnapshot> data = db.collection("ejercicios").whereEqualTo("dificultad", whereQ).get();
+        data.addOnSuccessListener(command -> {
+
+
+            List<VideoInfo> docList = command.toObjects(VideoInfo.class);
+            Random rand = new Random();
+            VideoInfo exercise = docList.get(rand.nextInt(docList.size()));
+            name = (TextView) getView().findViewById(R.id.textView6);
+            dificultad = (TextView) getView().findViewById(R.id.textView7);
+            img = (ImageView) getView().findViewById(R.id.imageView6);
+            name.setText(exercise.getNombre());
+            String diffName="";
+            switch (exercise.getDificultad()) {
+                case "basico":
+                    diffName = "Básico";
+                    break;
+                case "medio":
+                    diffName = "Intermedio";
+                    break;
+                case "avanzado":
+                    diffName = "Avanzado";
+                    break;
+            }
+            dificultad.setText("Dificultad: " + diffName);
+            Resources res = getResources();
+            Drawable drawable = ResourcesCompat.getDrawable(res, R.drawable.default_recomendado, null);
+            img.setImageDrawable(drawable);
+
+        });
 
         // final TextView textView = binding.textDashboard;
         // dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
